76faddd8299b93484093c139579b28caf2d10479
Elena <samodralolena@gmail.com>
2023-02-16 17:28:17 +0200

    main

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  Cpp_basic
-//
-//  Created by Samodral Elena on 16.02.2023.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
